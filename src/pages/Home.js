e790344ef54838942186ed1263fb32a872a3edb8
Ian Castillo <I.michaelcastillo@gmail.com>
2024-05-10 21:22:27 -0500

    uodate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="content">
-        <h1>Hey, I'm Ian.</h1>
+        <h1>Hey, I'm Ian!</h1>
         <p>I'm a designer, <a href="https://example.com/podcaster">podcaster</a>, writer, and software tinkerer. I'm currently building <a href="https://example.com/campsite">Campsite</a>, an app that brings together a team's posts, calls, notes, and chat.</p>
         <p>Before Campsite, I spent four years designing native mobile apps at <a href="https://github.com">GitHub</a>. Before GitHub, I co-founded <a href="https://spectrum.chat">Spectrum</a>, a platform for large-scale communities to have better public conversations. Spectrum was acquired by GitHub in November, 2018.</p>
         <p>Before Spectrum I designed payments experiences at Facebook, working across Facebook, Messenger, WhatsApp, and Instagram. I originally cut my teeth as the first product designer at <a href="https://buffer.com">Buffer</a>.</p>
