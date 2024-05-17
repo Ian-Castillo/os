95f1027b86ea0559c05a857761f77b37ab685714
Ian Castillo <I.michaelcastillo@gmail.com>
2024-05-16 20:16:45 -0500

    updated home.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.css'; // Make sure to link the CSS file
+import profileImage from '../ian.jpg'; // Adjust the path as necessary
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="content">
+        <img src={profileImage} alt="Ian Castillo" className="profile-image" />
         <h1>Hey, I'm Ian!</h1>
         <p>I'm a designer, <a href="https://github.com/ian-castillo">software engineer</a>, and entrepreneur. I'm currently building <a href="https://sunny.co/campsite">Sunny</a>, an app that re-imagines how we experience weather.</p>
         <p>Before Sunny, I spent time as the first designer and product manager at <a href="https://github.com">Argus.AI</a>. Before Argus, I was a product manager at <a href="https://riiid.com">Riiid</a>.</p>
