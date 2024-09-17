e1a9575004a4782dc46379c4b34cf970f15c1384
Ian Castillo <I.michaelcastillo@gmail.com>
2024-09-16 19:19:08 -0500

    time machince

--- src/pages/projects/TimeMachine.js
+++ src/pages/projects/TimeMachine.js
@@ -33,13 +33,12 @@ const TimeMachine = () => {
           </p>
           <p>This realization led me to create Time Machine Print (aka, Time Machine), a curated bookstore that's part digital-age oracle, part cultural time capsule. Each book featured is a window into a different world of ideas, a fragment of the larger story of human ingenuity and cultural evolution.
           </p>
-          <p>Whether you're a tech entrepreneur looking to humanize your next big idea, a designer seeking to infuse your work with global influences, or a creative professional aiming to bridge the gap between tradition and innovation, Time Machine Print is your launchpad.
-          </p>
         
         <p> The curated collection spans disciplines, eras, and cultures, offering insights from the trailblazers who came before us - their successes, their audacious failures, and the messy, beautiful process in between. I believe that understanding the past and present of global creativity is key to shaping its future.
 </p>
-        <p>At Time Machine, we're not just selling books; we're cultivating a community of curious minds ready to explore, experiment, and create. Because in this interconnected world, the next big idea might come from anywhere - a centuries-old artistic technique, a cutting-edge technological breakthrough, or a street food vendor's ingenious solution.
-        </p>
+      
+        <p>Whether you're a tech entrepreneur looking to humanize your next big idea, a designer seeking to infuse your work with global influences, or a creative professional aiming to bridge the gap between tradition and innovation, Time Machine Print is your launchpad.
+          </p>
         <p>To move forward, sometimes you need to step back, look around, and then leap in an unexpected direction. Let's take that journey together.</p>
         <p>Launching September 28th, 2024</p>
         </div>
